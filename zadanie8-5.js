07ee8dbbca085cf9efdef83b5cfea8ce970498a2
OmManiPemeHung <danielwraga@gmail.com>
2017-09-01 17:31:47 +0200

    first commit

---
+++ zadanie8-5.js
@@ -0,0 +1,7 @@
+var a = 1,
+	h = 5,
+	triangleArea;
+triangleArea = a*h/2
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+//var triangleArea = prompt('dane')
+alert('wynik, ' + triangleArea);
